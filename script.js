8e1a0e67d8aa313bcb5a6df0598e2196cfcf06ff
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-03-20 14:32:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-//your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-
-const imageUrls = [
-  { url: "https://picsum.photos/id/237/200/300" },
+ const imageUrls = [
+    { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
 
-function downloadImage(url){
+function downloadImage(imageObj){
     return new Promise((resolve, reject) => {
         const img = new Image();
         img.src = url;
         img.onload = () => resolve(img);
-        img.onerror = () => reject(`Failed to load image: ${img}`);
+        img.onerror = () => reject(`Failed to load image: ${imageObj.url}`);
     });
 }
 
