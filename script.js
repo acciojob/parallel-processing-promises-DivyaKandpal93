7d89e9dc5b8318bd8e87bd8d93fe3cf11a2760bf
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-03-20 13:02:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url){
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = url;
+        img.onload = () => resolve(img);
+        img.onerror = () => reject(`Failed to load image: ${img}`);
+    });
+}
+
+async function downloadImages() {
+    const loadingDiv = document.getElementById("loading");
+    const outputDiv = document.getElementById("output");
+    const errorDiv = document.getElementById("error");
+
+    loadingDiv.style.display = "block";
+    outputDiv,innerHTML = "";
+    errorDiv.innerHTML = "";
+
+    try{
+        const images = await Promise.all(imageUrls.map(downloadImage));
+
+    loadingDiv.style.display = "none";
+
+    images.forEach(img => outputDiv.appendChild(img));
+    }catch(error)
+    {
+        loadingDiv.style.display ="none";
+        errorDiv.innerHTML = error;
+    }
+    
+}
