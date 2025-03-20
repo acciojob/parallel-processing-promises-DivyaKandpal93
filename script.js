8c2410754952484f38d5db551c1cb4cd4c0e2003
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-03-20 14:10:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
-const images = [
+const imageUrls = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
